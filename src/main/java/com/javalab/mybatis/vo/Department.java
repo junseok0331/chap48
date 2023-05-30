66f0ba9f3328e0ebbb552f5d56432b0fe9c4dc76
505 <505@DESKTOP-O5STMU3>
2023-05-30 18:00:22 +0900

    최초 업로드

---
+++ src/main/java/com/javalab/mybatis/vo/Department.java
@@ -0,0 +1,17 @@
+package com.javalab.mybatis.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Department {
+	private String departmentId;
+	private String departmentName;
+}
